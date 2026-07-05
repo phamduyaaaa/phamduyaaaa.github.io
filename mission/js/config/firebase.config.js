9abfa8e10f69e8cea04b0910fbe1d11c68c849a7
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2026-07-05 17:37:20 +0700

    Create firebase.config.js

---
+++ mission/js/config/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSHMHJf2P-uxk_bKlKPTxFWsefAtjXVPE",
+  authDomain: "claude-mission-control.firebaseapp.com",
+  databaseURL: "https://claude-mission-control-default-rtdb.firebaseio.com",
+  projectId: "claude-mission-control",
+  storageBucket: "claude-mission-control.appspot.com",
+  messagingSenderId: "1011955299073",
+  appId: "1:1011955299073:web:abc123def456"
+};
